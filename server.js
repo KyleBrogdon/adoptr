61ecb864ce7d2a7d1042eb262f42020315ba66d6
rotorhead7700 <brogdonk@oregonstate.edu>
2022-07-22 16:50:29 -0500

    tinder card template

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const session = require("express-session");
+const cardFunctions = require("./views/pages/general/functions.js");
 
 const PORT = process.argv[2] || 3000; //sets port for site, default to 3000
 
---
+++ views/pages/general/functions.js
@@ -0,0 +1,12 @@
+// function animateCard(ev) {
+//     var t = ev.target;
+//     if (t.className === 'but-nope') {
+//       t.parentNode.classList.add('nope');
+//     }
+//     if (t.className === 'but-yay') {
+//       t.parentNode.classList.add('yes');
+//     }
+//   }
+//   document.body.addEventListener('click', animateCard);
+
+// module.exports = animateCard;
