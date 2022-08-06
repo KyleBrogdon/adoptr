c88a296cbe4109aef2148ff63a2130bf874bd178
Sriram Narayanan <sriram210@yahoo.in>
2022-08-06 12:48:56 -0700

    added pet search capabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,45 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-
-        'scripts/siteAdmin/users': path.join(__dirname, "src", "siteAdmin", "users.js"),
-        'scripts/siteAdmin/shelters': path.join(__dirname, "src", "siteAdmin", "shelters.js"),
-        'scripts/user/user': path.join(__dirname, "src", "user", "user.js"),
-        'scripts/pets/datingCards': path.join(__dirname, "src", "datingCards.js"),
-        'scripts/user/petProfile': path.join(__dirname, "src", "petProfile.js"),
-        'scripts/shelterAdmin/petProfile': path.join(__dirname, "src", "shelterAdmin","petProfiles.js"),
-        'scripts/shelterAdmin/shelterProfile':path.join(__dirname,"src","shelterAdmin","shelterProfile.js"),
-        'scripts/user/login': path.join(__dirname, "src", "login.js"),
-        'scripts/user/newUser': path.join(__dirname, "src", "newUser.js"),
-      },
-    mode: 'development',
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "[name].js",
-        publicPath: "/"
-    }, 
+  entry: {
+    "scripts/siteAdmin/users": path.join(
+      __dirname,
+      "src",
+      "siteAdmin",
+      "users.js"
+    ),
+    "scripts/siteAdmin/shelters": path.join(
+      __dirname,
+      "src",
+      "siteAdmin",
+      "shelters.js"
+    ),
+    "scripts/user/user": path.join(__dirname, "src", "user", "user.js"),
+    "scripts/pets/datingCards": path.join(__dirname, "src", "datingCards.js"),
+    "scripts/user/petProfile": path.join(__dirname, "src", "petProfile.js"),
+    "scripts/shelterAdmin/petProfile": path.join(
+      __dirname,
+      "src",
+      "shelterAdmin",
+      "petProfiles.js"
+    ),
+    "scripts/shelterAdmin/shelterProfile": path.join(
+      __dirname,
+      "src",
+      "shelterAdmin",
+      "shelterProfile.js"
+    ),
+    "scripts/user/login": path.join(__dirname, "src", "login.js"),
+    "scripts/user/newUser": path.join(__dirname, "src", "newUser.js"),
+    "scripts/user/petSearch": path.join(__dirname, "src", "petSearch.js"),
+  },
+  mode: "development",
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
 };
